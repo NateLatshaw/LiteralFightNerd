ddd5f71916d60f56566273bfcd9b76da1d4a429e
Nate Latshaw <dcms2015@gmail.com>
2021-02-19 00:43:52 -0500

    update first post

--- _site/posts/2021-02-19-introducing-judgeai/files/input_files/create_data.R
+++ _site/posts/2021-02-19-introducing-judgeai/files/input_files/create_data.R
@@ -5,14 +5,14 @@ library(data.table)
 library(lubridate)
 
 data_path <- 'E:/UFC-Analytics/MMA Decisions/Processed Decisions/'
-output_path <- './_posts/2021-02-03-predicting-ufc-judging-scores/predicting-ufc-judging-scores_files/'
+output_path <- './_posts/2021-02-19-introducing-judgeai/files/input_files/'
 
 # read in data
 df <- readRDS(paste0(data_path, 'round_decisions_and_stats.RDS'))
 
 # subset data
 df <- df[grepl('UFC', Event)]
-df <- df[year(EventDate) %in% 2010:2020]
+df <- df[year(EventDate) %in% 2011:2020]
 
 # save data
 current_dt <- gsub('-', '', Sys.Date())
