929a73be81276cd8b5eb2d8649ea4eaa285a4602
Nate Latshaw <dcms2015@gmail.com>
2022-01-05 23:07:07 -0500

    add post on visualizing fighter styles

---
+++ _supplemential_files/2022-01-05-visualizing-fighter-styles/input_files/create_data.R
@@ -0,0 +1,31 @@
+rm(list = ls())
+gc()
+
+library(data.table)
+library(lubridate)
+
+data_path <- 'E:/UFC-Analytics/Processed Data/'
+output_path <- './_supplemential_files/2022-01-05-visualizing-fighter-styles/input_files/'
+
+# read in data
+rounds <- fread(paste0(data_path, 'round_level_stats.csv'))
+fights <- fread(paste0(data_path, 'fight_level_stats.csv'))
+
+# set data types
+rounds[, EventDate := as.Date(EventDate)]
+fights[, EventDate := as.Date(EventDate)]
+
+# subset data
+rounds <- rounds[grepl('UFC', Event) | (grepl('The Ultimate Fighter', Event) & grepl('Finale', Event))]
+fights <- fights[grepl('UFC', Event) | (grepl('The Ultimate Fighter', Event) & grepl('Finale', Event))]
+
+all_fights <- copy(fights)
+all_fights <- all_fights[EventDate <= '2021-12-31']
+rounds <- rounds[year(EventDate) %in% 2012:2021]
+fights <- fights[year(EventDate) %in% 2012:2021]
+
+# save data
+current_dt <- gsub('-', '', Sys.Date())
+saveRDS(rounds, paste0(output_path, 'round_stats_', current_dt, '.RDS'))
+saveRDS(fights, paste0(output_path, 'fight_stats_', current_dt, '.RDS'))
+#saveRDS(all_fights, paste0(output_path, 'all_fight_stats_', current_dt, '.RDS'))
